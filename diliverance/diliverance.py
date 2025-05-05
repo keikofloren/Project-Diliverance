79dcb419a6423993281b91d9bc35afbf8c0f177c
Keiko <cakkeoang@gmail.com>
2025-05-05 20:40:01 +0800

    added about, events, team and donate page

--- diliverance/diliverance.py
+++ diliverance/diliverance.py
@@ -2,9 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/aboutdili')
+def about():
+    return render_template('about.html')
+
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
+@app.route('/meettheteam')
+def team():
+    return render_template('team.html')
+
+@app.route('/donate')
+def donate():
+    return render_template('donate.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
