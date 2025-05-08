19101b4ba4125885ea677730ba45eb151318362b
Keiko <cakkeoang@gmail.com>
2025-05-08 15:11:40 +0800

    update on home error

--- diliverance/diliverance.py
+++ diliverance/diliverance.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect(url_for('home'))
 
 @app.route('/home')
 def home():
